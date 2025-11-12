80a45eb3861960ada611114d64689d652b322e9a
Bepulleyang Evans Possible <125659283+RealestAtom@users.noreply.github.com>
2025-11-12 08:26:00 +0000

    Update client.py

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -16,3 +16,19 @@ class GithubOrgClient:
     def org(self):
         """Return JSON payload of organization from GitHub API"""
         return utils.get_json(f"https://api.github.com/orgs/{self.org_name}")
+
+    @property
+    def _public_repos_url(self):
+        """Return the repos_url from org payload"""
+        return self.org.get("repos_url")
+
+    def public_repos(self):
+        """Return list of repo names for the organization"""
+        url = self._public_repos_url
+        repos = utils.get_json(url)
+        return [repo["name"] for repo in repos]
+
+    def has_license(self, repo, license_key):
+        """Return True if repo has the given license"""
+        license_info = repo.get("license")
+        return license_info and license_info.get("key") == license_key
