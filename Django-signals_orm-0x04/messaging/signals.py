fc5d536024371198207f353093642888868a94a4
Bepulleyang Evans Possible <125659283+RealestAtom@users.noreply.github.com>
2025-11-30 16:14:27 +0000

    Update signals.py

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,35 +1,32 @@
-#!/usr/bin/env python3
-"""Signal handlers for cleanup and history."""
-
-from django.db.models.signals import pre_save, post_delete
-from django.dispatch import receiver
-from django.utils import timezone
-
+from rest_framework import serializers
 from .models import Message, MessageHistory
 
-
-@receiver(pre_save, sender=Message)
-def log_message_edit(sender, instance: Message, **kwargs):
-    if not instance.pk:
-        return
-    try:
-        old = Message.objects.get(pk=instance.pk)
-    except Message.DoesNotExist:
-        return
-
-    if old.content != instance.content:
-        editor = getattr(instance, "_current_user", instance.sender)
-        MessageHistory.objects.create(
-            message=instance,
-            old_content=old.content,
-            edited_by=editor,
-        )
-        instance.edited_at = timezone.now()
-        instance.edited_by = editor
+class MessageHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MessageHistory
+        fields = ['id', 'previous_content', 'logged_at']
 
 
-@receiver(post_delete, sender=User)
-def cleanup_user_data(sender, instance, **kwargs):
-    Message.objects.filter(sender=instance).delete()
-    Message.objects.filter(receiver=instance).delete()
-    MessageHistory.objects.filter(edited_by=instance).delete()
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ['id', 'sender', 'receiver', 'content', 'timestamp', 'parent_message']
+        read_only_fields = ['id', 'timestamp']
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    history = MessageHistorySerializer(many=True, read_only=True)
+    parent_message = serializers.PrimaryKeyRelatedField(queryset=Message.objects.all(), required=False, allow_null=True)
+    replies = ReplySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Message
+        fields = ['id', 'sender', 'receiver', 'content', 'created_at', 'timestamp', 'edited',
+                  'parent_message', 'history', 'replies']
+        read_only_fields = ['id', 'created_at', 'timestamp', 'edited', 'sender']
+
+    def update(self, instance, validated_data):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            instance.editor_override = request.user
+        return super().update(instance, validated_data)
